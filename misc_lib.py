4053206f55ffa3210c027b1f0dbdf558f85e55e2
Goro <goro@iMac.local>
2018-10-31 08:25:41 -0400

    Misc Lib additions

--- misc_lib.py
+++ misc_lib.py
@@ -1,11 +1,19 @@
+"""
+Miscellaneous needed library files
+
+"""
+
 import os
 
-def read_key_from_file():
+def read_key_from_file(filename):
     pass
 
-def base64_decode_exponent():
+def base64_decode_exponent(exponent):
     pass
 
-def base64_decode_modulus():
+def base64_decode_modulus(modulus):
+    pass
+
+def how_many_bytes_is_n(n):
     pass
 
