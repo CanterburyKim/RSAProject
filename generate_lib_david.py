99c75cff10fac3f0acbff4f8d5485f3016042792
sequ219 <12060219sequ@gmail.com>
2018-11-01 15:41:38 -0400

    other functions removed

---
+++ generate_lib_david.py
@@ -0,0 +1,35 @@
+"""
+Generate RSA Public and Private Keys library
+Contains functions that will allow for
+generation of public key, private key pair
+and then save those to file
+"""
+
+
+def calculate_n(p,q):
+    """
+    use the RSA formula to calculate n from p and q
+    TODO fill in the RSA formula here
+    """
+    # TODO: David
+    pass
+    # temp value for n TODO remove this and replace with correct
+    n = 100_000_980_001_501
+
+    # TODO fill RSA formula for n here
+
+
+    return n
+
+
+
+def generate_private_key(dec_exponent, modulus):
+    """
+    Use the decryption exponent and the modulus to create
+    a 2-tuple that contains those two
+    """
+    # TODO: David
+    # temp value.  TODO change this to correct
+    private_key = (1,1)
+
+    return private_key
