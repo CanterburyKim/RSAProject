4b9bc102eba20bea1ee8bb649ae139e8865da4d5
hujeninjaname69420 <slaycustoms@gmail.com>
2018-11-01 10:38:00 -0400

    edit out other members tasks

---
+++ decrypt_lib_joe.py
@@ -0,0 +1,20 @@
+
+    """
+    numbers is a list of numbers.
+    Convert each of those into a string,
+    put it into a list and return that list
+    """
+    # TODO: Joe
+
+    converted_string = 'convx'
+    return converted_string
+
+def assemble_message_from_chunks(message_chunks):
+    """
+    Take a long message and then split it into
+    5-character chunks.  Put those chunks into
+    a list and then return that list
+    """
+
+    assembled_message = 'hello I must be going'
+    return assembled_message
---
+++ decrypt_lib_test_joe.py
@@ -0,0 +1,45 @@
+"""
+The main test file for Decryption library
+
+"""
+
+import decrypt_lib
+
+
+def test_get_cipher_message():
+    # TODO: Tyler
+    pass
+
+def test_get_decryption_key(filename):
+    # TODO: Tyler
+
+    pass
+
+def test_rsa_decrypt_number(number, private_key):
+    # TODO: Chase
+    pass
+
+def test_rsa_decrypt_numbers(numbers, private_key):
+    # TODO: Tyler
+    pass
+
+def test_base64_decode_numbers(message_chunks):
+    # TODO: Tyler
+    pass
+
+def test_convert_number_to_message_chunk(number):
+    # TODO: Chase
+    pass
+
+def test_convert_numbers_to_strings(numbers):
+    # TODO: Joe
+    pass
+
+def test_assemble_message_from_chunks(message_chunks):
+    # TODO: Joe
+    pass
+
+# TODO put the actual test cases that you want to run here
+
+
+test_get_cipher_message()
