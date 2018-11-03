e3e5b24941fe48fafeb16a2d0c5018648b678960
Goro <evilninjax@gmail.com>
2018-11-03 08:30:44 -0400

    Adding Encrypt and Generate Unit Tests

--- generate_lib_test.py
+++ generate_lib_test.py
@@ -1,42 +1,50 @@
+"""
+Test cases for Generate Keys library
+"""
+
+import unittest
 from generate_lib import *
 
-def test_pick_two_random_prime():
-    # TODO: Leo
-    pass
+class TestGenerateLib(unittest.TestCase):
 
-def test_calculate_n():
-    # TODO: David
-    pass
+    def test_pick_two_random_prime():
+        # TODO: Leo
+        pass
 
-def test_calculate_phi():
-    # TODO: Nikita
-    pass
+    def test_calculate_n():
+        # TODO: David
+        pass
 
-def test_calculate_d():
-    # TODO: Alex
-    pass
+    def test_calculate_phi():
+        # TODO: Nikita
+        pass
 
-def test_generate_public_key():
-    # TODO: Leo
-    pass
+    def test_calculate_d():
+        # TODO: Alex
+        pass
 
-def test_generate_private_key():
-    # TODO: David
-    pass
+    def test_generate_public_key():
+        # TODO: Leo
+        pass
 
-def test_save_public_key_to_file():
-    # TODO: Alex
-    pass
+    def test_generate_private_key():
+        # TODO: David
+        pass
 
-def test_save_private_key_to_file():
-    # TODO: Nikita
-    pass
+    def test_save_public_key_to_file():
+        # TODO: Alex
+        pass
 
-def test_extended_euclidean_algorithm():
-    # TODO: Leo
-    pass
+    def test_save_private_key_to_file():
+        # TODO: Nikita
+        pass
 
-def test_build_prime_number_list():
-    # TODO: Nikita
+    def test_extended_euclidean_algorithm():
+        # TODO: Leo
+        pass
 
-test_calculate_phi()
+    def test_build_prime_number_list():
+        # TODO: Nikita
+
+if __name__=='__main__':
+    unittest.main()
