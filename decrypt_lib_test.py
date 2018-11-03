b206a6a5de04421d80e8b133e3e2e177531cb9b2
Goro <evilninjax@gmail.com>
2018-11-03 08:24:20 -0400

    Adding Unit Tests to the base Decryption Lib Test code

--- decrypt_lib_test.py
+++ decrypt_lib_test.py
@@ -1,45 +1,50 @@
 """
-The main test file for Decryption library
+Unit test file for Decryption
 
 """
-
+import unittest
 import decrypt_lib
 
+class TestDecryptMethods(unittest.TestCase):
+    """
+    Unit Test cases for the Decryption
+    methods
+    """
+    def test_get_cipher_message():
+        # TODO: Tyler
+        pass
 
-def test_get_cipher_message():
-    # TODO: Tyler
-    pass
+    def test_get_decryption_key(filename):
+        # TODO: Tyler
 
-def test_get_decryption_key(filename):
-    # TODO: Tyler
+        pass
 
-    pass
+    def test_rsa_decrypt_number(number, private_key):
+        # TODO: Chase
+        pass
 
-def test_rsa_decrypt_number(number, private_key):
-    # TODO: Chase
-    pass
+    def test_rsa_decrypt_numbers(numbers, private_key):
+        # TODO: Tyler
+        pass
 
-def test_rsa_decrypt_numbers(numbers, private_key):
-    # TODO: Tyler
-    pass
+    def test_base64_decode_numbers(message_chunks):
+        # TODO: Tyler
+        pass
 
-def test_base64_decode_numbers(message_chunks):
-    # TODO: Tyler
-    pass
+    def test_convert_number_to_message_chunk(number):
+        # TODO: Chase
+        pass
 
-def test_convert_number_to_message_chunk(number):
-    # TODO: Chase
-    pass
+    def test_convert_numbers_to_strings(numbers):
+        # TODO: Joe
+        pass
 
-def test_convert_numbers_to_strings(numbers):
-    # TODO: Joe
-    pass
+    def test_assemble_message_from_chunks(message_chunks):
+        # TODO: Joe
+        pass
 
-def test_assemble_message_from_chunks(message_chunks):
-    # TODO: Joe
-    pass
-
-# TODO put the actual test cases that you want to run here
+    # TODO put the actual test cases that you want to run here
 
 
-test_get_cipher_message()
+if __name__ == '__main__':
+    unittest.main()
