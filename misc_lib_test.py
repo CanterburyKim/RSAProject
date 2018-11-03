e84c00132b8e24eab57a2093152a6d81b32a64e6
Goro <evilninjax@gmail.com>
2018-11-03 08:32:34 -0400

    Added Misc Lib testcases

--- misc_lib_test.py
+++ misc_lib_test.py
@@ -1,1 +1,14 @@
+import unittest
 import misc_lib
+
+class TestMiscLib(unittest.TestCase):
+    def test_first(self):
+        pass
+
+
+    def test_second(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
