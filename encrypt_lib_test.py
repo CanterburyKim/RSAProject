c15731e86a943e41f6779e540e47c1fe43ed738f
K Murphy <kemurphy@canterburyschool.org>
2018-11-15 11:41:21 -0500

    Finished separation of plaintext into chunks

--- encrypt_lib_test.py
+++ encrypt_lib_test.py
@@ -1,41 +1,6 @@
-"""
-Encryption Test
-"""
+from encrypt_lib_kevin import *
 
-import encrypt_lib
 
-def test_get_message():
-    # TODO: Daniel
-    pass
-
-def test_get_encryption_key(filename):
-    # TODO: Xander
-    pass
-
-def test_split_message_into_chunks(message):
-    # TODO: Kevin
-    pass
-
-def test_convert_chunk_into_number(chunk_of_string):
-    # TODO: Daniel
-    pass
-
-def test_convert_chunks_into_numbers(chunks_of_strings):
-    # TODO: Kevin
-    pass
-
-def test_rsa_encrypt_number(number, public_key):
-    # TODO: Daniel
-    pass
-
-def test_rsa_encrypt_numbers(numbers, public_key):
-    # TODO: Xander
-    pass
-
-def test_base64_encode_numbers(numbers):
-    # TODO: Kevin
-    pass
-
-def test_save_encrypted_string(cipher_text):
-    # TODO: Xander
-    pass
+h = 'my name is kevin, hello there fk'
+q = split_message_into_chunks(h)
+print(h)
