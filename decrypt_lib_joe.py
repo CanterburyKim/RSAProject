1389a7e6328dc0369025f85c7ff3b251e67f2672
hujeninjaname69420 <slaycustoms@gmail.com>
2018-11-15 11:36:14 -0500

    I got assemble mess done and also found my convert numbers to string

--- decrypt_lib_joe.py
+++ decrypt_lib_joe.py
@@ -1,4 +1,12 @@
+def convert_numbers_to_strings(numbers):
+    """
+    numbers is a list of numbers.
+    Convert each of those into a string,
+    put it into a list and return that list
+    """
+    # TODO: Joe
 
+def temp_test():
     """
     numbers is a list of numbers.
     Convert each of those into a string,
@@ -9,12 +17,20 @@
     converted_string = 'convx'
     return converted_string
 
+
 def assemble_message_from_chunks(message_chunks):
+
     """
     Take a long message and then split it into
     5-character chunks.  Put those chunks into
     a list and then return that list
     """
+    assembled_message = ''
+    for chunck in message_chunks:
+        # print(chunck)
+        assembled_message+=chunck
 
-    assembled_message = 'hello I must be going'
+    print (assembled_message)
+
+
     return assembled_message
