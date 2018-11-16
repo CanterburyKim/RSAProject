648b0a0a9e0556f61aad253b58251b6206102a86
ACEinfinity7 <acelko7@gmail.com>
2018-11-16 10:02:37 -0500

    finished calculate d, it actually works this time

--- generate_lib_alex.py
+++ generate_lib_alex.py
@@ -10,6 +10,18 @@ def calculate_d(e, phi):
     #"""
     #given the e and phi use the RSA formula to calculate
     #d.
+    d_old = 0
+    r_old = phi
+    d_new = 1
+    r_new = e
+    while r_new > 0:
+        a = r_old // r_new
+        (d_old, d_new) = (d_new, d_old - a * d_new)
+        (r_old, r_new) = (r_new, r_old - a * r_new)
+    return d_old % phi if r_old == 1 else None
+
+
+
 
     #formula : e * d = 1 mod phi
     #"""
@@ -17,10 +29,10 @@ def calculate_d(e, phi):
     pass
     # TODO use the RSA method to calculate d from e and phi
 
-    for d in range(phi):
-        if (e * d) % phi == 1:
-            print ('d =', d)
-            print ('yes')
+    #for d in range(phi):
+    #    if (e * d) % phi == 1:
+    #        print ('d =', d)
+    #        print ('yes')
 
 
 
