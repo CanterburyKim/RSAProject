689ecbabb26835b3b77e7b3263306b25255170fb
K Murphy <kemurphy@canterburyschool.org>
2018-11-17 14:21:53 -0500

    functioning 'split_message_into_chunks' and functioning 'convert_chunks_into_numbers' definitions

--- encrypt_lib_test.py
+++ encrypt_lib_test.py
@@ -1,6 +1,11 @@
 from encrypt_lib_kevin import *
 
-
-h = 'my name is kevin, hello there fk'
+print('String to Chunks')
+h = 'my name is kevin. hello there'
 q = split_message_into_chunks(h)
 print(h)
+print(q)
+
+print('--------------------------Chunks to Num---------------------')
+d = convert_chunks_into_numbers(q)
+print(d)
