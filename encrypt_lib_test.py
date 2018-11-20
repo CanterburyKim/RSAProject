ebe67c698eed9fc0dac14e3a4249544df8f65ba2
K Murphy <kemurphy@canterburyschool.org>
2018-11-19 22:12:53 -0500

    Final Encrypt_Lib_Test

--- encrypt_lib_test.py
+++ encrypt_lib_test.py
@@ -1,6 +1,6 @@
 from encrypt_lib_kevin import *
 
-print('String to Chunks')
+print('------------------------String to Chunks--------------------')
 h = 'my name is kevin. hello there'
 q = split_message_into_chunks(h)
 print(h)
@@ -8,4 +8,9 @@ print(q)
 
 print('--------------------------Chunks to Num---------------------')
 d = convert_chunks_into_numbers(q)
+print(d)
+
+
+print('-------------------------Nums to Base64----------------------')
+d = base64_encode_numbers(d)
 print(d)
